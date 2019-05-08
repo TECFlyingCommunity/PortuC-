9099ad5b4a9ad068c08bd115b985495be4da90f2
Pedro Marinho <pedro.marinho238@gmail.com>
2019-05-07 22:17:38 -0400

    melhorias

--- portuC.h
+++ portuC.h
@@ -18,56 +18,75 @@
 #define PORTUC_H
 
 
-#define leia std::cin 
-#define escreva std::cout  
-#define pular std::endl
-#define inicio main
-#define inteiro int
-#define real float
-#define caractere char
-#define retorna return
-#define verdadeiro true
-#define falso false
-#define logico bool
-#define cadeia std::string
-#define se if
-#define senao else
-#define para for
-#define enquanto while
-#define faca do
-#define constante const
-#define escolha switch
-#define caso case
-#define pare break
-#define padrao default
-#define ou ||
-#define nao !
-#define diferente !=
-#define igual =
-#define mais +
-#define menos -
-#define mult *
-#define div /
-#define e &&
-#define vazio void
-#define sistema system 
-#define classe class
-#define publico public
-#define privado private
-#define executar run 
-#define dublo double 
-#define longo long
-#define emlinha inline 
-#define curto short
-#define estatico static 
-#define continuar continue
-#define novo new 
-#define deletar delete  
-#define este this
-#define protegido protected
-#define tentar try
-#define lancar throw
-#define pegar catch
+#define leia std::cin //ok
+#define escreva std::cout  //ok
+#define pular std::endl //ok
+#define inicio main //ok
+#define sistema system //ok
+#define executar run  //ok
+
+#define inteiro int //ok
+#define real float //ok
+#define caractere char // ok
+#define logico bool // ok
+#define cadeia std::string //ok
+#define vazio void //ok
+#define dublo double  // ok
+#define longo long //ok
+#define constante const // ok
+#define curto short // ok
+#define estatico static // ok
+#define externo extern
+#define registrar register
+#define assinado signed
+#define volatil volatile
+
+#define verdadeiro true //ok
+#define falso false // ok
+
+#define retorna return // ok
+#define se if // ok
+#define senao else //ok
+#define para for // ok
+#define enquanto while //ok
+#define faca do // ok
+#define escolha switch //pk
+#define caso case // ok
+#define pare break // ok
+#define continuar continue //ok
+#define padrao default // ok
+
+#define ou || // ok
+#define nao ! // ok
+#define diferente != //ok
+#define igual = //ok
+#define mais + //ok
+#define menos - //ok
+#define mult * //ok
+#define div / //ok
+#define e && //ok
+
+#define classe class //ok
+#define publico public //ok
+#define privado private //ok
+#define protegido protected //ok
+#define amiga friend
+
+#define operador operator
+
+
+#define tamanho sizeof
+#define modelo template
+#define uniao union
+
+#define emlinha inline // ok
+#define novo new  //ok
+#define deletar delete //  ok
+#define este this //ok
+
+#define tentar try //ok
+#define lancar throw //ok
+#define pegar catch //ok
 
 
 
